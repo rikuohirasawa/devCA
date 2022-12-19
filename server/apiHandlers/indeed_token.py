6b6a9b7817063493f750f316d653953a7ac43775
rikuohirasawa <rikuoh84@gmail.com>
2022-12-19 16:54:14 -0330

    scraper setup contd

--- server/apiHandlers/indeed_token.py
+++ server/apiHandlers/indeed_token.py
@@ -2,6 +2,11 @@ import requests
 from dotenv import load_dotenv, dotenv_values
 import os
 
+import datetime
+
+now = datetime.date.today()
+print(now)
+
 # load env variables to get access token
 # load_dotenv()
 # client_id = os.getenv('INDEED_CLIENT_ID')
@@ -20,11 +25,11 @@ import os
 
 # get_access_token()
 
-url = "https://auth.emsicloud.com/connect/token"
+# url = "https://auth.emsicloud.com/connect/token"
 
-payload = "client_id=%3CCLIENT_ID%3E&client_secret=%3CCLIENT_SECRET%3E&grant_type=client_credentials&scope=jobs"
-headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+# payload = "client_id=%3CCLIENT_ID%3E&client_secret=%3CCLIENT_SECRET%3E&grant_type=client_credentials&scope=jobs"
+# headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
-response = requests.request("POST", url, data=payload, headers=headers)
+# response = requests.request("POST", url, data=payload, headers=headers)
 
-print(response.text)
+# print(response.text)
--- server/apiHandlers/scraper.py
+++ server/apiHandlers/scraper.py
@@ -1,33 +1,45 @@
 import requests
 import bs4
-import json
+import datetime
+import time
 # using rapid api to make calls to scrapeninja, a scraping api that bypasses cloudflare 403 errors 
 # https://pixeljets.com/blog/bypass-cloudflare/
-developer_skills = ['javascript, html, css, python, sql, java, node.js, typescript, c#, bash, c++']
-regions = ['alberta', 'british%20columbia', 'manitoba', 'new%20brunswick', 'newfoundland%20and%20labrador', 'northwest%20territories', 'nova%20scotia', 'nunavut', 'ontario', 'prince%20edward%20island', 'quebec', 'saskatchewan', 'yukon']
+developer_skills = ['Javascript', 'Python']
+# , 'HTML', 'CSS', 'Python', 'SQL', 'Java']
+# , 'Node.js', 'Typescript', 'C#', 'Bash', 'C++', 'PHP', 'C', 'PowerShell', 'Go', 'Kotlin', 'Rust', 'Ruby', 'Dart', 'Assembly', 'Swift', 'R', 'VBA', 'Matlab', 'Groovy', 'Objective-C', 'Scala', 'Perl', 'Haskell', 'Delphi', 'Clojure', 'Elixir', 'LISP', 'Julia', 'F#', 'Erlang', 'APL', 'Crystal', 'COBOL'
+regions = ['alberta', 'british%20columbia']
+#, 'manitoba', 'new%20brunswick', 'newfoundland%20and%20labrador'
+# , 'northwest%20territories', 'nova%20scotia', 'nunavut', 'ontario', 'prince%20edward%20island', 'quebec', 'saskatchewan', 'yukon'
 
+# class SkillsData:
+# 	def __init__(self, regions):
+# 		self.technology = regions
 
-response = requests.get('https://www.adzuna.ca/search?q=python&w=newfoundland')
-# parse response with bs4
-soup = bs4.BeautifulSoup(response.text, 'html.parser')
-# select tag that contains quanitity of jobs
-html_tag = soup.select_one('span.mr-1')
-# select number value and convert to int
-num_value = int(html_tag.get('data-cy-count'))
+# class RegionData: 
+# 	def __init__(self, skills):
+# 		self.region = skills
 
-class RegionData:
-	def __init__(self, technology):
-		self.technology = technology
+region_data = {'date': datetime.date.today()}
+for region in regions:
+	total_job_count = 0
+	region_data[region] = {}
+	for skill in developer_skills:
+		response = requests.get(f'https://www.adzuna.ca/search?q={skill}&w={region}')
+		# parse response with bs4
+		soup = bs4.BeautifulSoup(response.text, 'html.parser')
+		# select tag that contains quanitity of jobs
+		html_tag = soup.select_one('span.mr-1')
+		# select number value and convert to int
+		num_value = int(html_tag.get('data-cy-count'))
+		region_data[region][skill] = num_value
+		total_job_count += num_value
+		# add 1s delay in loop iterations to not overload their servers when scraping - i'm sure they are fine but just in case :)
+		time.sleep(1)
+	region_data[region]['total_job_count'] = total_job_count
+	time.sleep(1)
 
-class TechnologyData: 
-	def __init__(self, region):
-		self.region = region
+print(region_data)
 
-# for skill in developer_skills:
-# 	for region in regions:
-# 		response = requests.get(f'https://www.adzuna.ca/search?q={skill}&w={region}')
-# 		soup = bs4.BeautifulSoup(response.text, 'html.parser')
-# 		jobs_available = soup.title.split(' ', 1)
 
 
 
